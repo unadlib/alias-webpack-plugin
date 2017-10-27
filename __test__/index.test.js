1925978aa0165bcc94958a008f8b6174655e0beb
Michael Lin <unadlib@gmail.com>
2017-10-27 13:46:20 +0800

    update default alias config file `.alias` or `.alias.js`

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,6 +1,6 @@
 import path from 'path'
-import { exec } from 'child_process'
-import aliasWebpackPlugin, { resolvePath } from '../dist'
+import { exec, execSync } from 'child_process'
+import aliasWebpackPlugin, { resolvePath, getAliasPath } from '../dist'
 
 test(`The custom alias file is empty`, () => {
   expect(aliasWebpackPlugin('./test.json')).toEqual({})
@@ -14,15 +14,22 @@ test(`Defult alias file is empty`, () => {
   expect(aliasWebpackPlugin()).toEqual({})
 })
 
-test(`Defult  Alias file is exist`, () => {
-  const execProcess = exec('mv ./__test__/testboo/alias.json ./alias.json && npx babel-node ./__test__/testboo/boo.js', (error, stdout, stderr) =>{
+test(`Defult  'alias.json' alias file is exist`, () => {
+  execSync('mv ./__test__/testboo/alias.json ./alias.json && npx babel-node ./__test__/testboo/boo.js', (error, stdout, stderr) =>{
     expect(JSON.parse(stdout).test).toEqual(
       path.resolve(path.resolve(__dirname, '../'), require('../alias.json').test)
     )
   })
-  execProcess.on('exit',()=>{
-    exec('mv ./alias.json ./__test__/testboo/alias.json')
-  });
+  execSync('mv ./alias.json ./__test__/testboo/alias.json')
+})
+
+test(`Defult 'alias.config.js' alias file is exist`, () => {
+  execSync('mv ./__test__/testboo/alias.config.js ./alias.config.js && npx babel-node ./__test__/testboo/boo.js', (error, stdout, stderr) =>{
+    expect(JSON.parse(stdout).test).toEqual(
+      path.resolve(path.resolve(__dirname, '../'), require('../alias.config.js').test)
+    )
+  })
+  execSync('mv ./alias.config.js ./__test__/testboo/alias.config.js')
 })
 
 test(`Test 'resolvePath' function`, () => {
@@ -30,3 +37,27 @@ test(`Test 'resolvePath' function`, () => {
     path.resolve(__dirname, '../a.js')
   )
 })
+
+test(`Test 'getAliasPath' function`, () => {
+  expect(getAliasPath([])).toEqual(undefined)
+})
+
+test(`Test 'resolvePath' function`, () => {
+  expect(getAliasPath(['../test.json'])).toEqual(
+    undefined
+  )
+})
+
+test(`Test 'resolvePath' function`, () => {
+  expect(getAliasPath(['./__test__/test.json'])).toEqual(
+    path.resolve(__dirname, './test.json')
+  )
+})
+
+test(`Test 'resolvePath' function`, () => {
+  expect(getAliasPath([undefined,'./__test__/test.json'])).toEqual(
+    path.resolve(__dirname, './test.json')
+  )
+})
+
+
--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
 import fs from 'fs'
 import path from 'path'
 
-const DEFAULT_FILE_PATH = './alias.json'
+const DEFAULT_ALIAS_PATH = [
+  './alias.json',
+  './alias.config.js'
+]
 
 export const resolvePath = (relativePath) => {
   const currentDirectory = fs.realpathSync(process.cwd())
   return path.resolve(currentDirectory, relativePath)
 }
 
-export default (filePath = DEFAULT_FILE_PATH) => {
-  const aliasPath = resolvePath(filePath)
-  const isExists = fs.existsSync(aliasPath)
+export const getAliasPath = (aliasFileList) => {
+  let aliasPath
+  for (const aliasFile of aliasFileList) {
+    if (aliasFile) {
+      const aliasFilePath = resolvePath(aliasFile)
+      const isExists = fs.existsSync(aliasFilePath)
+      if (isExists) {
+        aliasPath = aliasFilePath
+        break
+      }
+    }
+  }
+  return aliasPath
+}
+
+export default (filePath) => {
+  const aliasPath = getAliasPath([
+    filePath,
+    ...DEFAULT_ALIAS_PATH,
+  ])
   const aliasSetting = {}
-  if (isExists) {
+  if (aliasPath) {
     const aliasOptions = require(aliasPath)
     Object.entries(aliasOptions).forEach(([key, relativePath]) => {
       aliasSetting[key] = resolvePath(relativePath)
